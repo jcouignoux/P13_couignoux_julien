4fdce63aa7e1bfa82d4833f7ddae43a5ad387b49
Julien Couignoux <j.couignoux@gmail.com>
2022-09-28 11:33:19 +0200

    migrate app oc_lettings_site to new app website

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(('website.urls', 'website'), namespace='website')),
     path('lettings/', include(('lettings.urls', 'lettings'), namespace='lettings')),
     path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('admin/', admin.site.urls),
--- website/views.py
+++ website/views.py
@@ -12,4 +12,4 @@ Praesent volutpat porttitor magna, non finibus neque cursus id."""
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
