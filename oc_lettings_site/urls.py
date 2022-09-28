9148f7b2572b7ba2b5e9700cc6e0a921b77bed1f
Julien Couignoux <j.couignoux@gmail.com>
2022-09-28 10:23:48 +0200

    migrate profiles to new apps profiles

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include(('lettings.urls', 'lettings'), namespace='lettings')),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', include(('profiles.urls', 'profiles'), namespace='profiles')),
     path('admin/', admin.site.urls),
 ]
